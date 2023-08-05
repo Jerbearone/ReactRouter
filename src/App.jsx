3aedb2e559173fa6dfc0ac89211497d8e1c02d22
= <jwparker284@gmail.com>
2023-08-05 07:44:43 -0700

    added routes and base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <h1></h1>
+      <div id="navbar">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/blue"}>Blue</Link>
+        <Link to={"/red"}>Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/' element={Home()}></Route>
+          <Route path='/blue' element={Blue()}></Route>
+          <Route path='/red' element={Red()}></Route>
+
+        </Routes>
+      </div>
     </div>
   )
 }
